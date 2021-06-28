bc02e65146cce720808bda32e71e41c96090eff6
Parham Javadi <pjfooeve09@yahoo.com>
2021-06-28 16:04:04 -0700

    commit tree 03f382e11d51090badd7d3754e24b250c8f8e61d

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ] 
+
+function superbowlWin(arrOfObjects) {
+    //debugger;
+    //Use find() to test each Object to see if the result is "W" — a win!
+    //return the year when the win occurred (if it occurred at all!). If no win is found, it should return, sadly, undefined
+    let win = arrOfObjects.find(obj => {
+      return obj.result === "W";
+    });
+  
+    return win ? win.year : undefined;
+  }
